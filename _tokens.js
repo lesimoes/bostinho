06ecee553e3c69fd8284ce080a67f83f0e16c022
Leandro Simões <lesimoes0@gmail.com>
2018-04-01 11:54:32 -0300

    fixed untracked files

--- _tokens.js
+++
@@ -1,8 +0,0 @@
-module.exports.telegram = "TELEGRAM TOKEN"
-module.exports.firebase = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: ""}
