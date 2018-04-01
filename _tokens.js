3729ff92f23dfd6414748b180c162ee0640f2bec
Leandro Simões <lesimoes0@gmail.com>
2018-04-01 11:45:55 -0300

    Arquivo de tokens

---
+++ _tokens.js
@@ -0,0 +1,8 @@
+module.exports.telegram = "TELEGRAM TOKEN"
+module.exports.firebase = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""}
